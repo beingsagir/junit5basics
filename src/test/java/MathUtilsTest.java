8ceb34752551777ce18407b1a6c1debb9b479cc4
Sagiruddin Mondal <sagir.sagir@gmail.com>
2019-06-10 14:45:40 +0530

    before and after methods and some annotations

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,14 +1,28 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
     MathUtils mathUtils;
+
+    @BeforeAll
+    static void startBeforeInitialization(){
+        System.out.println("start even before the creation of the current class");
+    }
 
     @BeforeEach
     void init() {
         mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void end(){
+        System.out.println("Test is done");
     }
 
     @Test
@@ -20,6 +34,36 @@ class MathUtilsTest {
     @Test
     void testDivide(){
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should through arithmetic exception");
+    }
+
+    @Test
+    @DisplayName("test simple divide in divideTest")
+    void testDivideTwo(){
+        int expected = 2;
+        assertEquals(expected, mathUtils.divide(4, 2), "Divide by zero should through arithmetic exception");
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    @DisplayName("test simple divide in linux only")
+    void testDivideOnlyForLinux(){
+        int expected = 2;
+        assertEquals(expected, mathUtils.divide(4, 2), "Divide by zero should through arithmetic exception");
+    }
+
+    @Test
+    @DisplayName("test simple divide with assumptions")
+    void testDivideAssumptions(){
+        assumeTrue(1==1);
+        int expected = 2;
+        assertEquals(expected, mathUtils.divide(4, 2), "Divide by zero should through arithmetic exception");
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("TDD should not fell")
+    void testFail(){
+        fail("manual fail");
     }
 
     @Test
